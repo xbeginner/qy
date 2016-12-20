ecc00fc2900092e8dce7694730a53f1386318f1e
x_coder <internet_408@126.com>
2016-12-20 13:17:48 +0800

    qy1219

--- app/src/main/java/com/xph/qyxy/MainActivity.java
+++ app/src/main/java/com/xph/qyxy/MainActivity.java
@@ -1,13 +1,38 @@
 package com.xph.qyxy;
 
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.drawer_layout)
+    DrawerLayout mDrawerLayout;
+    @BindView(R.id.nav_view)
+    NavigationView navigationView;
+
+    @BindView(R.id.news_activity_tablayout)
+    TabLayout tabLayout;
+    @BindView(R.id.news_activity_viewpager)
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //绑定ButterKnife
+        ButterKnife.bind(this);
+
+        //initView();
     }
 }
